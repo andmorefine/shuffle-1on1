3199bcc58c20b613450a0299523f1735915eb4c9
tanaka.satoshi <tanaka.satoshi@a-tm.co.jp>
2021-09-10 10:49:28 +0900

    add favicon

--- components/header.js
+++ components/header.js
@@ -5,7 +5,10 @@ const Header = () => (
     <header>
       <Navbar bg="light" expand="lg">
         <Container fluid>
-          <Navbar.Brand href="/">シャッフル</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img alt="" src="/site-tile.png" width="30" height="30" className="d-inline-block align-top" />
+            <span className="px-2">シャッフル1on1</span>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import Footer from './footer'
 import { Container } from 'react-bootstrap'
 
 export default function Layout({ children, title }) {
-  const defaultTitle = 'シャッフル'
+  const defaultTitle = 'シャッフル1on1'
   const headTitle = title ? `${title} | ${defaultTitle}` : defaultTitle
 
   return (
@@ -15,6 +15,8 @@ export default function Layout({ children, title }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
         <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
         <script src="https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
       </Head>
 
